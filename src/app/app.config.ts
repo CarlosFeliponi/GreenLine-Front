ee817dbd0ff01355a7b75088d130c6429120ec13
CarlosFeliponi <carlosdmf.42@gmail.com>
2024-05-19 00:14:18 -0300

    chore: add MDB-angular-ui-kit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,8 @@ import { provideRouter } from '@angular/router';
 
 import { provideHttpClient } from '@angular/common/http';
 import { routes } from './app.routes';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideHttpClient()]
+  providers: [provideRouter(routes), provideHttpClient(), provideAnimations()]
 };
