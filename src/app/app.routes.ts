efce1e14e0aec30208cbb3c9a79da55429d80366
Gabriel Pletsch <gabrielexxonpletsch@gmail.com>
2024-05-21 14:41:22 -0300

    ADD component Itens_Carrinho

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { CarrinhoComponent } from './components/carrinho/carrinho.component';
+import { CarrinhoCardComponent } from './components/carrinho/components/carrinho-card/carrinho-card.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"", redirectTo: "carrinho", pathMatch: 'full'},
+    {path: "carrinho", component: CarrinhoComponent},
+    {path: "carrinhoCard", component: CarrinhoCardComponent}
+];
---
+++ src/app/components/carrinho/components/carrinho-card/carrinho-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-carrinho-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './carrinho-card.component.html',
+  styleUrl: './carrinho-card.component.scss'
+})
+export class CarrinhoCardComponent {
+
+}
