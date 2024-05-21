9e89d41145ddeb05a2982f685482d267762f5693
Gabriel Pletsch <gabrielexxonpletsch@gmail.com>
2024-05-21 18:13:51 -0300

    ADD Card footer and Resumo pedido in Carrinho

--- src/app/components/carrinho/carrinho.component.ts
+++ src/app/components/carrinho/carrinho.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from '../layout/footer/footer.component';
+import { CarrinhoCardComponent } from './components/carrinho-card/carrinho-card.component';
 
 @Component({
   selector: 'app-carrinho',
   standalone: true,
-  imports: [FooterComponent, RouterOutlet],
+  imports: [FooterComponent, RouterOutlet, CarrinhoCardComponent],
   templateUrl: './carrinho.component.html',
   styleUrl: './carrinho.component.scss'
 })
