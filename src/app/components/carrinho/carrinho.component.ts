71f22fb8726607a3579391f0eab67f30821a99f0
Gabriel Pletsch <gabrielexxonpletsch@gmail.com>
2024-05-21 17:07:19 -0300

    ADD routes and footer

--- src/app/components/carrinho/carrinho.component.ts
+++ src/app/components/carrinho/carrinho.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from '../layout/footer/footer.component';
 
 @Component({
   selector: 'app-carrinho',
   standalone: true,
-  imports: [],
+  imports: [FooterComponent, RouterOutlet],
   templateUrl: './carrinho.component.html',
   styleUrl: './carrinho.component.scss'
 })
