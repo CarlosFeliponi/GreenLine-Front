01f5ffe3d4068c2087294ce9c56aab35c0733204
gustavo <gustavoronn@gmail.com>
2024-05-22 16:07:03 -0300

    add: model login

---
+++ src/app/models/login.ts
@@ -0,0 +1,9 @@
+export class Login {
+    emailUsuario: string;
+    senhaUsuario: string;
+  
+    constructor(emailUsuario: string, senhaUsuario: string) {
+      this.emailUsuario = emailUsuario;
+      this.senhaUsuario = senhaUsuario;
+    }
+  }
