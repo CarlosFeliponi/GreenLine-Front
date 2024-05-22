80f098bebdafd6a7cb059011949a1f8047612259
lucas schneider <lucaseduschneiderengs@gmail.com>
2024-05-22 09:52:01 -0400

    feat add services/produtos/produto-service

---
+++ src/app/services/produtos.service.ts
@@ -0,0 +1,38 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Produto } from '../models/produto';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProdutosService {
+
+  http = inject(HttpClient);
+
+  API = "http://localhost:8080/api/produto";
+
+
+  constructor() { }
+
+  listAll(): Observable<Produto[]>{
+    return this.http.get<Produto[]>(this.API + "/listAll");
+  }
+
+  delete(id: number): Observable<string>{
+    return this.http.delete<string>(this.API + "/delete/" + id, {responseType: 'text' as 'json'});
+  }
+
+  save(produto: Produto): Observable<string>{
+    return this.http.post<string>(this.API + "/save", produto, {responseType: 'text' as 'json'});
+  }
+
+  update(produto: Produto, id: number): Observable<string>{
+    return this.http.put<string>(this.API + "/update/"+id, produto, {responseType: 'text' as 'json'});
+  }
+
+  findById(id: number): Observable<Produto>{
+    return this.http.get<Produto>(this.API + "/findById/"+id);
+  }
+  
+}
