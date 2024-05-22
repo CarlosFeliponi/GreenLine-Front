eb60736cf38dde34c9b1fd7ac2ac257e8b1dbd57
gustavo <gustavoronn@gmail.com>
2024-05-22 16:08:14 -0300

    add: services login

---
+++ src/app/services/login.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Login } from '../models/login';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+
+  private API = 'http://localhost:8080/api/usuario/login'; // URL do endpoint de login no backend
+
+  constructor(private http: HttpClient) { }
+
+  login(loginData: Login): Observable<Login> {
+    console.log('Dados de login:', loginData);
+    return this.http.post<Login>(this.API, loginData); // Envie o objeto Login completo no corpo da solicitação
+  }
+}
