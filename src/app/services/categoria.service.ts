6c119c0f0081cbd32ca1f4dcbac0180a58d648b1
lucas schneider <lucaseduschneiderengs@gmail.com>
2024-05-23 01:24:38 -0400

    feat: add services/categoria

---
+++ src/app/services/categoria.service.ts
@@ -0,0 +1,37 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { Categoria } from '../models/categoria';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoriaService {
+
+  http = inject(HttpClient);
+
+  API = "http://localhost:8080/api/categoria";
+  
+  constructor() { }
+
+  listAll(): Observable<Categoria[]>{
+    return this.http.get<Categoria[]>(this.API + "/listAll");
+  }
+
+  delete(id: number): Observable<string>{
+    return this.http.delete<string>(this.API + "/delete/" + id, {responseType: 'text' as 'json'});
+  }
+
+  save(categoria: Categoria): Observable<string>{
+    return this.http.post<string>(this.API + "/save", categoria, {responseType: 'text' as 'json'});
+  }
+
+  update(categoria: Categoria, id: number): Observable<string>{
+    return this.http.put<string>(this.API + "/update/"+id, categoria, {responseType: 'text' as 'json'});
+  }
+
+  findById(id: number): Observable<Categoria>{
+    return this.http.get<Categoria>(this.API + "/findById/"+id);
+  }
+  
+}
