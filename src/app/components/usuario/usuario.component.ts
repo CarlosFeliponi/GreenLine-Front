f62abfba19790747245e2e38d074dcb22574528f
gustavo <gustavoronn@gmail.com>
2024-05-23 15:14:28 -0300

    feat (usuario): usuario/component

---
+++ src/app/components/usuario/usuario.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-usuario',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './usuario.component.html',
+  styleUrl: './usuario.component.scss'
+})
+export class UsuarioComponent {
+  
+}
