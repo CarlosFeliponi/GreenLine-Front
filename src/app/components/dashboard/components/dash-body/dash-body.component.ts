4d34a0117412137b52370a86af0749a31141df96
lucas schneider <lucaseduschneiderengs@gmail.com>
2024-05-23 16:23:13 -0400

    feat(admin): add componets/dashboar/dash-body

---
+++ src/app/components/dashboard/components/dash-body/dash-body.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dash-body',
+  standalone: true,
+  imports: [],
+  templateUrl: './dash-body.component.html',
+  styleUrl: './dash-body.component.scss'
+})
+export class DashBodyComponent {
+
+}
