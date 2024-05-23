60d263e1090504b2fedd682dbf93bdfffda9bd1c
Gabriel Pletsch <gabrielexxonpletsch@gmail.com>
2024-05-23 13:52:11 -0300

    testes-2

--- src/app/components/carrinho/components/carrinho-card/carrinho-card.component.ts
+++ src/app/components/carrinho/components/carrinho-card/carrinho-card.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Carrinho } from '../../../../models/carrinho/carrinho';
+import { CommonModule } from '@angular/common';
+import { CarrinhoService } from '../../../../services/carrinho/carrinho.service';
+import { RouterLink } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-carrinho-card',
   standalone: true,
-  imports: [ ],
+  imports: [RouterLink, CommonModule ],
   templateUrl: './carrinho-card.component.html',
   styleUrl: './carrinho-card.component.scss'
 })
 export class CarrinhoCardComponent {
+
+  lista: Carrinho[] = [];
+
+
+  carrinhoService = inject(CarrinhoService);
 
 }
--- src/app/components/carrinho/components/carrinho-item/carrinho-item.component.ts
+++ src/app/components/carrinho/components/carrinho-item/carrinho-item.component.ts
@@ -18,6 +18,7 @@ export class CarrinhoItemComponent {
 
   carrinhoService = inject(CarrinhoService);
 
+
   constructor() {
     this.listAll();
   }
