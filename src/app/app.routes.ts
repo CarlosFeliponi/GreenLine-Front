8032caaf1c984360fb39235a3a31397473c2dd94
CarlosFeliponi <carlosdmf.42@gmail.com>
2024-05-23 19:54:24 -0300

    ajustando rotas pasando para dean, banco

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,11 @@ import { ProdutosCardComponent } from './components/produtos/components/produtos
 import { ProdutosComponent } from './components/produtos/produtos.component';
 import { LoginComponent } from './components/login/login.component';
 import { CadastroComponent } from './components/cadastro/cadastro.component';
+import { UsuarioComponent } from './components/usuario/usuario.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { Produto } from './models/produto';
+import { ProdutoslistComponent } from './components/produtos/produtoslist/produtoslist.component';
+import { CategoriaslistComponent } from './components/categorias/categoriaslist/categoriaslist.component';
 
 export const routes: Routes = [
     
@@ -16,8 +21,12 @@ export const routes: Routes = [
     {path: "star", component: PrincipalUserComponent, children: [
         {path: "home", component: HomeComponent},
         {path: "produto", component: ProdutosComponent},
-        
-        
-        
+        {path: "perfil", component: UsuarioComponent},
+    ]},
+
+    {path: "x", redirectTo: "admin", pathMatch: 'full'},
+    {path: "admin", component: DashboardComponent, children: [
+        {path: "produtolist", component: ProdutoslistComponent},
+        {path: "categorialist", component: CategoriaslistComponent},
     ]}
 ];
--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { DashNavComponent } from './components/dash-nav/dash-nav.component';
 import { DashBodyComponent } from './components/dash-body/dash-body.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [DashNavComponent, DashBodyComponent],
+  imports: [DashNavComponent, DashBodyComponent, RouterOutlet],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
