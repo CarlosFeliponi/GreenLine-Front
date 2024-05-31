7e9056e4b04570c58d7adab5e1cb4520a76ac815
CarlosFeliponi <carlosdmf.42@gmail.com>
2024-05-31 01:35:42 -0300

    refactor(home): automating carousel and adding link in the button

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,15 +1,24 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { MdbCarouselModule } from 'mdb-angular-ui-kit/carousel';
-import { FooterComponent } from '../layout/footer/footer.component';
-import { NavbarComponent } from '../layout/navbar/navbar.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MdbCarouselModule],
+  imports: [MdbCarouselModule, RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-
+  actions = [
+    {
+      img: 'home_1.png'
+    },
+    {
+      img: 'home_2.png'
+    },
+    {
+      img: 'home_3.png'
+    }
+  ]
 }
