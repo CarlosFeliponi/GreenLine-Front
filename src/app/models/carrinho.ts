0a176a47632b526179e6d8e0705dd4d72fa06f72
CarlosFeliponi <carlosdmf.42@gmail.com>
2024-05-30 22:53:43 -0300

    refactor: add more properties

--- src/app/models/carrinho.ts
+++ src/app/models/carrinho.ts
@@ -1,7 +1,13 @@
+import { ItemCarrinho } from "./item-carrinho";
+import { Usuario } from "./usuario";
+
 export class Carrinho {
     idCarrinho!: number;
     descricaoCarrinho!: string;
     valorCarrinho!: number;
+    itemCarrinho!: ItemCarrinho[];
+    usuario!: Usuario;
+    status!: string;  
 
     constructor(idCarrinho: number, descricaoCarrinho:string, valorCarrinho:number){
         this.descricaoCarrinho = descricaoCarrinho;
