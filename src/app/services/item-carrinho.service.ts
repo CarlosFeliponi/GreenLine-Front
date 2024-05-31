7f3b2b8b40e7708f92c53a6634c2300c7004ef1d
CarlosFeliponi <carlosdmf.42@gmail.com>
2024-05-30 23:04:38 -0300

    feat: add methods *save*, *update* and *getCarrinhoByUser*

--- src/app/services/item-carrinho.service.ts
+++ src/app/services/item-carrinho.service.ts
@@ -3,6 +3,7 @@ import { Injectable, inject } from '@angular/core';
 import { ItemCarrinho } from '../models/item-carrinho';
 
 import { Observable } from 'rxjs';
+import { Carrinho } from '../models/carrinho';
 
 @Injectable({
   providedIn: 'root'
@@ -19,12 +20,19 @@ export class ItemCarrinhoService {
     return this.http.get<ItemCarrinho[]>(this.API+"/listAll");
   }
 
+  save(itemCarrinho: ItemCarrinho): Observable<string>{
+    return this.http.post<string>(this.API + "/save", itemCarrinho, {responseType: 'text' as 'json'});
+  }
+  
   delete(id: number): Observable<string>{
     return this.http.delete<string>(this.API+"/delete/"+id, {responseType: 'text' as 'json'});
   }
 
-  // updateQuantity(id: number, quantProd: number): Observable<Carrinho> {
-  //   return this.http.put<Carrinho>(${this.API}/update/${id}, { quantProd });
-  // }
+  update(itemCarrinho: ItemCarrinho, id: number): Observable<string>{
+    return this.http.put<string>(this.API + "/update/"+id, itemCarrinho, {responseType: 'text' as 'json'});
+  }
 
+  getCarrinhoByUser(idUsuario: number): Observable<Carrinho>{
+    return this.http.get<Carrinho>(this.API+"/getCarrinhoByUser?idUsuario="+idUsuario);
+  }
 }
