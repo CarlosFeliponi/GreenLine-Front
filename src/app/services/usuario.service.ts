f82549d1071e44d27e642208695ecf741ffeb804
gustavo <gustavoronn@gmail.com>
2024-06-05 22:04:37 -0300

    working...

--- src/app/services/usuario.service.ts
+++ src/app/services/usuario.service.ts
@@ -1,16 +1,19 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { Cadastro } from '../models/cadastro'; // Ajuste o caminho conforme necessário
 import { Usuario } from '../models/usuario';
+import { LoginService } from './login.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsuarioService {
-   API_CADASTRO = 'http://localhost:8080/api/usuario';
    API_USUARIO = 'http://localhost:8080/api/cliente';
 
+   http = inject(HttpClient);
 
-  constructor() { }
+  findByUsuarioId(idUsuarioLogado: number): Observable<Usuario>{
+    return this.http.get<Usuario>(this.API_USUARIO+"/findByUsuarioId/"+idUsuarioLogado);
+  }
+
 }
