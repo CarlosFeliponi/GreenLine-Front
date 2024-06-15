4c4fea6721ae027014b0f3144dcabcd3f578a296
Gustavo Ronan <129910842+gustavoronan@users.noreply.github.com>
2024-06-15 12:32:00 -0300

    Refactor/cliente usuario (#21)
    * working...
    
    * feat(login) funcao de autenticacao adicionada
    
    * feat (login) funcao indByLoginId adicionada
    
    * feat(usuario) adicionada funcoes para cadastrar mais informacoes do usuario (se trata do cliente do back)
    
    * working
    
    * feat retirando duplicacoes no html de login e usuario

--- src/app/models/usuario.ts
+++ src/app/models/usuario.ts
@@ -1,16 +1,11 @@
+import { Login } from "./login";
+
 export class Usuario {
-    idCliente: number;
-    enderecoCliente: string;
-    nomeCliente: string;
-    telefoneCliente: string;
-    usuarioIdUsuario: number;
-  
-    constructor(idCliente: number, enderecoCliente: string, nomeCliente: string, telefoneCliente: string, usuarioIdUsuario: number) {
-      this.idCliente = idCliente;
-      this.enderecoCliente = enderecoCliente;
-      this.nomeCliente = nomeCliente;
-      this.telefoneCliente = telefoneCliente;
-      this.usuarioIdUsuario = usuarioIdUsuario;
-    }
+    idCliente!: number;
+    enderecoCliente!: string;
+    nomeCliente!: string;
+    telefoneCliente!: string;
+    usuario!: Login;
+    
   }
   
--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -10,12 +10,20 @@ export class LoginService {
 
   public usuarioLogado!: Login;
 
-  private API = 'http://localhost:8080/api/usuario/login'; // URL do endpoint de login no backend
+  private API = 'http://localhost:8080/api/usuario'; // URL do endpoint de login no backend
 
   constructor(private http: HttpClient) { }
 
   login(loginData: Login): Observable<Login> {
     console.log('Dados de login:', loginData);
-    return this.http.post<Login>(this.API, loginData); // Envie o objeto Login completo no corpo da solicitação
+    return this.http.post<Login>(this.API+"/login", loginData); // Envie o objeto Login completo no corpo da solicitação
   }
+
+  
+  findByLoginId(idLoginLogado: number): Observable<Login>{
+    return this.http.get<Login>(this.API+"/findByLoginId/"+idLoginLogado);
+  }
+
+
+
 }
