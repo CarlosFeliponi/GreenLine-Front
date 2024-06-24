9fd990890a7d7cf4190c3cccd658a4f64cdd3341
lucas schneider <lucaseduschneiderengs@gmail.com>
2024-06-24 01:09:55 -0400

    refator(listagem-clientes) correção de erros de codigo

--- src/app/components/listagem-clientes/listagem-clientes.component.ts
+++ src/app/components/listagem-clientes/listagem-clientes.component.ts
@@ -1,9 +1,9 @@
 import { Component, TemplateRef, ViewChild, inject } from '@angular/core';
-import { Cliente } from '../models/cliente';
+import { Cliente } from '../../models/cliente';
 import { Title } from 'chart.js';
 import Swal from 'sweetalert2';
 import { MdbModalRef, MdbModalService } from 'mdb-angular-ui-kit/modal';
-import { ClienteService } from '../services/cliente.service';
+import { ClienteService } from '../../services/cliente.service';
 
 @Component({
   selector: 'app-listagem-clientes',
@@ -39,39 +39,40 @@ export class ListagemClientesComponent {
     this.listAll();
   }
 
-  delete(cliente: Cliente) {
-    Swal.fire({
-      title: 'Atenção',
-      text: 'Deseja deletar o cliente?',
-      icon: 'warning',
-      showConfirmButton: true,
-      showDenyButton: true,
-      confirmButtonText: 'Sim',
-      denyButtonText: 'Não',
-    }).then((result) => {
-      if (result.isConfirmed) {
+  // metodo para deletar um cliente. Não necessario para o caso de uso da aplicação
+  // delete(cliente: Cliente) {
+  //   Swal.fire({
+  //     title: 'Atenção',
+  //     text: 'Deseja deletar o cliente?',
+  //     icon: 'warning',
+  //     showConfirmButton: true,
+  //     showDenyButton: true,
+  //     confirmButtonText: 'Sim',
+  //     denyButtonText: 'Não',
+  //   }).then((result) => {
+  //     if (result.isConfirmed) {
 
 
-        this.clienteService.delete(cliente.idCliente).subscribe({
-          next: mensagem => {
-            Swal.fire({
-              title: 'SUCESSO',
-              text: mensagem,
-              icon: 'success',
-              confirmButtonText: 'OK',
-            });
-            this.listAll();
-          },
-          error: erro => {
-            Swal.fire({
-              title: 'ERRO',
-              text: 'Ocorreu um erro inesperado',
-              icon: 'error',
-              confirmButtonText: 'OK',
-            });
-          }
-        });
-      }
-    });
-  }
+  //       this.clienteService.delete(cliente.idCliente).subscribe({
+  //         next: mensagem => {
+  //           Swal.fire({
+  //             title: 'SUCESSO',
+  //             text: mensagem,
+  //             icon: 'success',
+  //             confirmButtonText: 'OK',
+  //           });
+  //           this.listAll();
+  //         },
+  //         error: erro => {
+  //           Swal.fire({
+  //             title: 'ERRO',
+  //             text: 'Ocorreu um erro inesperado',
+  //             icon: 'error',
+  //             confirmButtonText: 'OK',
+  //           });
+  //         }
+  //       });
+  //     }
+  //   });
+  // }
 }
