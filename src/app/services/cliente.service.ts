a9bbdfb7969acd11614d5051c13e28d4acdbefca
oMaquito1000 <pedrohenriquefoletto@gmail.com>
2024-06-23 21:37:47 -0300

    feat add listagem_clientes_component

---
+++ src/app/services/cliente.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Cliente } from '../models/cliente';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ClienteService {
+
+  http = inject(HttpClient);
+
+  API = "http://localhost:8080/api/cliente";
+
+  constructor() { }
+
+  listAll(): Observable<Cliente[]>{
+    return this.http.get<Cliente[]>(this.API+"/listAll");
+  }
+
+  delete(id: number): Observable<string>{
+    return this.http.delete<string>(this.API + "/delete/" + id, {responseType: 'text' as 'json'});
+  }
+}
