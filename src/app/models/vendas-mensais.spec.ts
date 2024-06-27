68344a79792ccbc6d01dd527831d973ca4aa6702
gustavo <gustavoronn@gmail.com>
2024-06-27 12:29:25 -0300

    delete arquivos de testes deletados para deploy

--- src/app/models/vendas-mensais.spec.ts
+++
@@ -1,7 +0,0 @@
-import { VendasMensais } from './vendas-mensais';
-
-describe('VendasMensais', () => {
-  it('should create an instance', () => {
-    expect(new VendasMensais()).toBeTruthy();
-  });
-});
