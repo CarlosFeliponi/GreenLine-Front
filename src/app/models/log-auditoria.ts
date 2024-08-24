aa7e6f184c3463804339e0bf25bac84cd0d9dce3
Saler C. Lourenço <saler.cez@gmail.com>
2024-08-24 20:17:11 -0300

    cria model e service para o log

---
+++ src/app/models/log-auditoria.ts
@@ -0,0 +1,9 @@
+export class LogAuditoria {
+    idLog!:number;
+	emailUsuario!: string;
+	LocalDateTime!: Date;
+	acao!: string;
+	entity!: string;
+	idEntity!: number;
+	detalhes!: string;
+}
---
+++ src/app/services/log-auditoria.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { Observable } from 'rxjs';
+import { LogAuditoria } from '../models/log-auditoria';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LogAuditoriaService {
+
+  
+  http = inject(HttpClient);
+
+  API = environment.SERVIDOR+"/api/logauditoria";
+
+  constructor() { }
+
+  listAll(): Observable<LogAuditoria[]>{
+    return this.http.get<LogAuditoria[]>(this.API+"/listAll");
+  }
+}
