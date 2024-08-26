c95fdcc90818edb01000eabd2469b7dfb64fbb2b
Saler C. Lourenço <saler.cez@gmail.com>
2024-08-26 14:50:46 -0300

    adiciona filtragem por role

--- src/app/components/log-auditoria/log-auditoria.component.ts
+++ src/app/components/log-auditoria/log-auditoria.component.ts
@@ -5,14 +5,14 @@ import Swal from 'sweetalert2';
 import { MdbAccordionModule } from 'mdb-angular-ui-kit/accordion';
 
 // import para o calendario
-import {MatIconModule} from '@angular/material/icon';
-import {MatDatepickerModule} from '@angular/material/datepicker';
-import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 // import para o input de ordem
-import {FormsModule} from '@angular/forms';
-import {MatInputModule} from '@angular/material/input';
-import {MatSelectModule} from '@angular/material/select';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 
 //----------
 interface Ordem {
@@ -22,68 +22,94 @@ interface Ordem {
 @Component({
   selector: 'app-log-auditoria',
   standalone: true,
-  imports: [MdbAccordionModule, MatFormFieldModule, MatDatepickerModule, MatIconModule, MatSelectModule, MatInputModule, FormsModule ],
+  imports: [
+    MdbAccordionModule,
+    MatFormFieldModule,
+    MatDatepickerModule,
+    MatIconModule,
+    MatSelectModule,
+    MatInputModule,
+    FormsModule,
+  ],
   templateUrl: './log-auditoria.component.html',
-  styleUrl: './log-auditoria.component.scss'
+  styleUrl: './log-auditoria.component.scss',
 })
 export class LogAuditoriaComponent {
-  //ajudar ou deletar, datos que estao nas opc de ORDEN
-  ordens: Ordem[] = [
-    {value: 'ordem-0', viewValue: 'CrescenteX'},
-    {value: 'ordem-1', viewValue: 'DecrescenteX'}
-  ];
-
+  public params = {
+    startDate: undefined,
+    endDate: undefined,
+    acao: undefined,
+    roleUsuario: undefined,
+    logName: undefined,
+    emailUsuario: undefined
+  }
   selected = 'option1';
 
   // startDate: Date | null = null;
   // endDate: Date | null = null;
 
   logService = inject(LogAuditoriaService);
-  
-  logAuditoria: LogAuditoria[] =[];
+
+  logAuditoria: LogAuditoria[] = [];
   ordem: boolean = true;
-  
-  constructor(){
+
+  constructor() {
     this.listAll();
   }
-  
-  listAll(){
-    this.logService.listAll().subscribe(
-      {
-        next: lista =>{
-          this.logAuditoria = lista;
-        },
-        error: erro => {
-          console.log("Erro: ", erro);
-          Swal.fire({
-            title: 'Erro',
-            icon: 'error',
-            confirmButtonText: 'Ok',
-          });
-        }
-      }
-    );
+
+  listAll() {
+    this.logService.listAll().subscribe({
+      next: (lista) => {
+        this.logAuditoria = lista;
+      },
+      error: (erro) => {
+        console.log('Erro: ', erro);
+        Swal.fire({
+          title: 'Erro',
+          icon: 'error',
+          confirmButtonText: 'Ok',
+        });
+      },
+    });
   }
-  sortLog(){
+
+  sortLog() {
     console.log(this.ordem);
-    if(this.ordem){
-      this.logAuditoria.sort(function(a, b){
-        if(a.timestamp < b.timestamp)
-          return -1;
-        if(a.timestamp > b.timestamp)
-          return 1;
+    if (this.ordem) {
+      this.logAuditoria.sort(function (a, b) {
+        if (a.timestamp < b.timestamp) return -1;
+        if (a.timestamp > b.timestamp) return 1;
         return 0;
       });
-    }
-    else{
-      this.logAuditoria.sort(function(a, b){
-        if(a.timestamp > b.timestamp)
-          return -1;
-        if(a.timestamp < b.timestamp)
-          return 1;
+    } else {
+      this.logAuditoria.sort(function (a, b) {
+        if (a.timestamp > b.timestamp) return -1;
+        if (a.timestamp < b.timestamp) return 1;
         return 0;
       });
     }
     console.log('log', this.logAuditoria);
+  }
+
+  findLogsByCriterio() {
+    this.logService
+      .findLogsByCriterio(
+        this.params.startDate, // startDate
+        this.params.endDate, // endDate
+        this.params.acao, // acao
+        this.params.roleUsuario, // roleUsuario
+        this.params.logName, // logName
+        this.params.emailUsuario // emailUsuario
+      )
+      .subscribe({
+        next: (data: LogAuditoria[]) => {
+          this.logAuditoria = data;
+          console.log('Logs fetched successfully:', this.logAuditoria);
+          console.log('role', this.params.roleUsuario)
+        },
+        error: (error) => {
+          console.error('Error fetching logs:', error);
+        },
+      });
   }
 }
