b7a23d7f15efbdc96a8e5850d4420f716fe64383
CarlosFeliponi <carlosdmf.42@gmail.com>
2024-08-26 12:26:55 -0300

    feat(header) add os imports necessario para os componentes do header no log
    - alguns pseudo codigos para manter a estrutura basica dos componentes
    - remover os pseudo codigos apos se conectar com o back

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,7 +6,8 @@ import { routes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { meuhttpInterceptor } from './auth/http-interceptor.service';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideNativeDateAdapter } from '@angular/material/core';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimations(), provideHttpClient(withInterceptors([meuhttpInterceptor])), provideAnimationsAsync()]
+  providers: [provideRouter(routes), provideAnimations(), provideHttpClient(withInterceptors([meuhttpInterceptor])), provideAnimationsAsync(), provideNativeDateAdapter()]
 };
--- src/app/components/log-auditoria/log-auditoria.component.ts
+++ src/app/components/log-auditoria/log-auditoria.component.ts
@@ -3,23 +3,40 @@ import { LogAuditoria } from '../../models/log-auditoria';
 import { LogAuditoriaService } from '../../services/log-auditoria.service';
 import Swal from 'sweetalert2';
 import { MdbAccordionModule } from 'mdb-angular-ui-kit/accordion';
-import { MdbDropdownModule } from 'mdb-angular-ui-kit/dropdown';
 
+// import para o calendario
+import {MatIconModule} from '@angular/material/icon';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
+// import para o input de ordem
+import {FormsModule} from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+
+//----------
+interface Ordem {
+  value: string;
+  viewValue: string;
+}
 @Component({
   selector: 'app-log-auditoria',
   standalone: true,
-  imports: [MdbAccordionModule, MdbDropdownModule],
+  imports: [MdbAccordionModule, MatFormFieldModule, MatDatepickerModule, MatIconModule, MatSelectModule, MatInputModule, FormsModule ],
   templateUrl: './log-auditoria.component.html',
   styleUrl: './log-auditoria.component.scss'
 })
 export class LogAuditoriaComponent {
+  //ajudar ou deletar, datos que estao nas opc de ORDEN
+  ordens: Ordem[] = [
+    {value: 'ordem-0', viewValue: 'CrescenteX'},
+    {value: 'ordem-1', viewValue: 'DecrescenteX'}
+  ];
 
-  logService = inject(LogAuditoriaService);
+  selected = 'option1';
 
-  logCompleto: LogAuditoria[] = [];
-  logSortido: LogAuditoria[] =[];
-  ordem: boolean = true;
+  // startDate: Date | null = null;
+  // endDate: Date | null = null;
 
   constructor(){
     this.listAll();
